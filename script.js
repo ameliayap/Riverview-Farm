a83b8c32273dde2cfb9e54291c7ba721a68d7814
Amelia Yap <amelia_ylh@hotmail.com>
2021-01-08 05:59:03 +0000

    Updated phone and address hyperlinks

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 15, 2021 15:37:25").getTime();
+const SUBMIT_FORM_URL = "https://us-west2-riverviewfarm.cloudfunctions.net/send-contact-email";
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+/* This is for preloader chicken animation */
+//Giving preloader enough time to render
+$(window).on("load",function(){
+$('#status').delay(300).fadeOut(); 
+$('#preloader').delay(300).fadeOut('slow');
+$('body').delay(550).css({'overflow':'visible'});
+});
 
-  // Get todays date and time
-  var now = new Date().getTime();
+/* This is for the sticky navbar */
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-  // Display the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
   }
-}, 1000);
+}
+
+$('#contact-form').on('submit', function(e){
+  const data = $('#contact-form').serializeJSON();
+  e.preventDefault();
+  $.ajax({
+    type: "POST",
+    processData: false,
+    url: SUBMIT_FORM_URL,
+    crossDomain: true,
+    contentType: "application/json; charset=utf-8",
+    data: JSON.stringify(data),
+    success: function(res) {
+      $('#contact-form').trigger("reset");
+      alert('Thank you! Your message has been successfully sent.');
+    },
+    error: function(error) {
+      console.log(error.status);
+      console.log(error.responseText);
+      alert(`Oops! There was a problem sending your message. ${error.status}: ${error.responseText}`);
+    }
+  });
+});
